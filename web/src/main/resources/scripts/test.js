78ccc1589545b5254f337cdee124ede77b12b2cb
dengzepeng <ycdzp@live.com>
2017-11-18 13:03:48 +0800

    javascript引擎nashorn测试

---
+++ web/src/main/resources/scripts/test.js
@@ -0,0 +1,1 @@
+var fun = function(name) {print('java对象属性： ' + name);};
---
+++ web/src/test/java/com/skyline/web/JsTest.java
@@ -0,0 +1,37 @@
+package com.skyline.web;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class JsTest {
+    public static void main(String args[]) throws IOException, ScriptException, NoSuchMethodException {
+
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        Resource resource = new ClassPathResource("/scripts/test.js");
+        InputStream inputStream = resource.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String jsStr = reader.readLine();
+        reader.close();
+        inputStream.close();
+
+        System.out.println(jsStr);
+        engine.eval(jsStr);
+
+        Invocable invocable = (Invocable) engine;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("姓名","邓泽鹏");
+        jsonObject.put("年龄","26");
+        jsonObject.put("性别","男");
+        invocable.invokeFunction("fun",jsonObject);
+    }
+}
