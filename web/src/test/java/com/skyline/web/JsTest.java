db9ff5614d69a40682cfaf7d209cc18eb37fc291
dengzepeng <ycdzp@live.com>
2017-11-18 18:52:36 +0800

    update

--- web/src/test/java/com/skyline/web/JsTest.java
+++ web/src/test/java/com/skyline/web/JsTest.java
@@ -8,15 +8,13 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URL;
 
 public class JsTest {
     public static void main(String args[]) throws IOException, ScriptException, NoSuchMethodException {
 
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        /*ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
         Resource resource = new ClassPathResource("/scripts/test.js");
         InputStream inputStream = resource.getInputStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
@@ -34,11 +32,34 @@ public class JsTest {
         System.out.println(stringBuilder);
         engine.eval(stringBuilder.toString());
 
-        Invocable invocable = (Invocable) engine;
+        Invocable invocable = (Invocable) engine;*/
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("姓名","邓泽鹏");
         jsonObject.put("年龄","26");
         jsonObject.put("性别","男");
+        //invocable.invokeFunction("fun",jsonObject);
+
+
+
+
+
+        //http://orpz1adxx.bkt.clouddn.com/test.js
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        Invocable invocable = (Invocable) engine;
+        URL url = new URL("http://orpz1adxx.bkt.clouddn.com/test.js");
+        InputStream is = new BufferedInputStream(url.openStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String str = null;
+        while ((str = reader.readLine()) != null){
+            stringBuilder.append(str);
+        }
+
+        reader.close();
+        is.close();
+
+        engine.eval(stringBuilder.toString());
         invocable.invokeFunction("fun",jsonObject);
     }
 }
