efbce76a0c775a50cbd529b97e25e811b78916b8
dengzepeng <ycdzp@live.com>
2017-11-18 14:44:15 +0800

    update

--- web/src/main/java/com/skyline/WebApplication.java
+++ web/src/main/java/com/skyline/WebApplication.java
@@ -2,10 +2,8 @@ package com.skyline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.skyline.service","com.skyline.repository","com.skyline.entity"})
 public class WebApplication {
 
 	public static void main(String[] args) {
