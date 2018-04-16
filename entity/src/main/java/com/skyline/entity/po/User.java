a3eb778e1d535f2e8252b61136c0b40e078051b4
dengzepeng <dengzp@253.com>
2018-04-16 14:26:20 +0800

    update

--- entity/src/main/java/com/skyline/entity/po/User.java
+++ entity/src/main/java/com/skyline/entity/po/User.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "t_user")
@@ -20,4 +21,8 @@ public class User implements Serializable {
     private String username;
     @Column(name = "password")
     private String password;
+    @Column(name = "mobile",length = 11)
+    private String mobile;
+    @Column(name = "create_time")
+    private LocalDate createTime;
 }
